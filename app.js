9df06387aedd8b21a82856f76319e128bff45a09
Chris Bautista <chrisbautista@Cabby.local>
2014-09-26 16:40:19 -0700

    removed default layout 1

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+function checkTime(i) {
+    i = (i < 1) ? 0 : i;
+    if (i < 10) i = "0" + i;  // add zero in front of numbers < 10
+    return i;
+}
+
+var tmPromise, myApp = angular.module('codeSpud',[]);
+
+myApp.service('initService', function($rootScope) {
+    //initialize schedule list
+    this.experiments = {
+        list: [
+            ['/experiments/cbTimer/','AngularJS: Timer']
+        ]
+    };
+});
+
+myApp.controller('homePageCtrl', ['$scope', 'initService', function ($scope, initService) {
+
+    $scope.experiments = initService.experiments;
+
+}]);
+
