b9e3d767375748ed21fb09ddf9048d292e647bd4
Chris Bautista <christopher.bautista@gmail.com>
2014-10-18 16:28:43 -0700

    refactored timer experiment

--- experiments/cbTimer/public_html/app.js
+++ experiments/cbTimer/public_html/app.js
@@ -1,23 +1,18 @@
 'use strict';
 
-
-function checkTime(i) {
-    i = (i < 1) ? 0 : i;
-    if (i < 10) i = "0" + i;  // add zero in front of numbers < 10
-    return i;
-}
-
-var tmPromise, myApp = angular.module('cbTimer',[]);
+var tmPromise, myApp = angular.module('cbTimer', []);
 
 /**
  * @ngservice initService
  * @param $rootScope
  */
-myApp.service('initService', function($rootScope) {
-    //initialize schedule list
-    this.timeSchedule = {
-        history: []
-    };
+myApp.service('initService', function () {
+
+  //initialize schedule list
+  this.timeSchedule = {
+    history: []
+  };
+
 });
 
 /**
@@ -25,78 +20,80 @@ myApp.service('initService', function($rootScope) {
  *  @params  $scope, $timeout, $initService
  *  handle toggling between modes, and binding data
  */
-myApp.controller('cbTimerCtrl', ['$scope', '$timeout','initService', function ($scope, $timeout, initService) {
+myApp.controller('cbTimerCtrl', ['$scope', '$timeout', 'initService', function ($scope, $timeout, initService) {
 
-    $scope.timeStart = 0;
-    $scope.timeEnd = 0;
-    $scope.timeSchedule = initService.timeSchedule;
+  var timeStart = 0, timeEnd = 0;
+  $scope.timeSchedule = initService.timeSchedule;
+  $scope.mode = "Start";
+  $scope.timer = "00:00:00";
+
+
+  function checkTime(i) {
+    i = (i < 1) ? 0 : i;
+    if (i < 10) { i = "0" + i; }  // add zero in front of numbers < 10
+    return i;
+  }
+
+  /**
+  *  @func startTimer
+  *  trigger timer to start, 
+  *  recursive, call again when timer expires
+  */
+  function startTimer() {
+    // toggle
+    $scope.mode = "Stop";
+
+    var h, m, s, ms, today = new Date();
+    // compute for the duration, 
+    // normalize for the user
+    timeEnd = today.getTime();
+    ms = Math.floor((timeEnd - timeStart) / 1000);
+    h =  checkTime(Math.floor(ms / 3600));
+    ms = Math.floor(ms % 3600);
+    m = checkTime(Math.floor(ms / 60));
+    ms = Math.floor(ms % 60);
+    s = checkTime(Math.floor(ms));
+    // normalize time string
+    $scope.timer = h + ":" + m + ":" + s;
+
+    // timer expired, restart timer
+    tmPromise = $timeout(function () {
+      startTimer();
+    }, 1000);
+  }
+
+  /**
+   * @func stopTimer
+   * handle end of timer
+   */
+  function stopTimer() {
+    // toggle
     $scope.mode = "Start";
-    $scope.timer = "00:00:00";
 
-    /**
-     *  @func $scope.startTimer
-     *  trigger timer to start, 
-     *  recursive, call again when timer expires
-     */
-    $scope.startTimer = function () {
-        // toggle
-        $scope.mode = "Stop";
-        
-        // compute for the duration, 
-        // normalize for the user
-        var today = new Date();
-        $scope.timeEnd = today.getTime();
-        var ms = Math.floor(($scope.timeEnd - $scope.timeStart)/1000);
-        var h =  checkTime(Math.floor(ms/3600));
-            ms = Math.floor(ms%3600);
-        var m = checkTime(Math.floor(ms/60));
-            ms = Math.floor(ms%60);
-        var s = checkTime(Math.floor(ms));
-        // normalize time string
-        $scope.timer = h + ":" + m + ":" + s;
-        
-        // timer expired, restart timer
-        tmPromise = $timeout(function () {
-            $scope.startTimer();
-        }, 500);
+    // stop timeout service
+    $timeout.cancel(tmPromise);
 
-    };
+    // add to history
+    $scope.timeSchedule.history.push([timeStart,
+                                      timeEnd,
+                                      (timeEnd - timeStart) / 1000]);
+  }
 
-    /**
-     * @func $scope.stopTimer
-     * handle end of timer
-     */
-    $scope.stopTimer = function () {
-        var dt = new Date();
-        
-        // toggle
-        $scope.mode = "Start";
-        
-        // stop timeout service
-        $timeout.cancel(tmPromise); 
-        
-        // add to history
-        $scope.timeSchedule.history.push([$scope.timeStart,
-                                          $scope.timeEnd,
-                                          ($scope.timeEnd-$scope.timeStart)/1000]);
-    };
+  /***  @func $scope.toggleTimer
+   *  toggle between modes
+   */
+  $scope.toggleTimer = function () {
 
-    /**
-     *  @func $scope.toggleTimer
-     *  toggle between modes
-     */    
-    $scope.toggleTimer = function () {
+    // handle modes
+    if ($scope.mode === 'Start') {
+      var today = new Date();
+      timeStart = today.getTime();
+      startTimer();
+    } else {
+      stopTimer();
+    }
 
-        // handle modes
-        if ($scope.mode === 'Start') {
-            var today = new Date();
-            $scope.timeStart = today.getTime(),
-            $scope.startTimer();
-        } else {
-            $scope.stopTimer();
-        }
-
-    };
+  };
 
 
 }]);
