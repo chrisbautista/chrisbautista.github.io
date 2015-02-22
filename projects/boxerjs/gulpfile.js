53c3239946b613b2fb8f2085296f121d3278f07f
Chris Bautista <christopher.bautista@gmail.com>
2015-02-21 16:45:04 -0800

    updated projects/youscentit/src/Application.js

---
+++ projects/boxerjs/gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var notify = require('gulp-notify');
+var htmlreplace = require('gulp-html-replace');
+var uglifycss = require('gulp-uglifycss');
+
+var project = "boxerjs";
+
+gulp.task('js', function () {
+   gulp.src('src/js/*.js') //select all javascript files under js/ and any subdirectory
+        .pipe(concat(project+'.min.js')) //the name of the resulting file
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/'))
+        .pipe(notify({ message: 'Finished minifying JavaScript'}));
+});
+
+gulp.task('css', function () {
+   gulp.src(['src/css/app.css']) //select all javascript files under js/ and any subdirectory
+        .pipe(concat('project.min.css')) //the name of the resulting file
+        .pipe(uglifycss())
+        .pipe(gulp.dest('build/css/'))
+        .pipe(notify({ message: 'Finished minifying CSS'}));
+});
+
+gulp.task('copy', function() {
+    gulp.src('src/assets/**/*')
+        .pipe(gulp.dest('build/assets/'));
+    gulp.src('src/css/vendor/**/*')
+        .pipe(gulp.dest('build/css/vendor/'));
+    gulp.src('src/js/vendor/**/*')
+        .pipe(gulp.dest('build/js/vendor/'));
+    gulp.src(['src/postcard.tpl'])
+        .pipe(gulp.dest('build/'));
+    gulp.src('src/index.html')
+        .pipe(htmlreplace({
+            'css':'css/'+project+'.min.css',
+            'js': 'js/'+project+'.min.js'
+        }))
+        .pipe(gulp.dest('build/'))
+        .pipe(notify({ message: 'Finished copying files'}));
+});
+
+gulp.task('default', function () {
+  gulp.run('copy');
+  gulp.run('js');
+  gulp.run('css');
+
+  gulp.watch('src/js/*.js', function () {
+        gulp.run('js');
+    });
+  gulp.watch('src/css/*.css', function () {
+        gulp.run('css');
+    });
+  gulp.watch('src/*', function () {
+        gulp.run('copy');
+  });
+});
