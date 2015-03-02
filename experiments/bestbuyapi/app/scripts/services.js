9e083dd7295d6e1a2fcd07fd58c1f73f53f90504
Chris Bautista <christopher.bautista@gmail.com>
2015-03-02 10:44:27 -0800

    fixed http URL error on Timer experiments

--- experiments/bestbuyapi/app/scripts/services.js
+++ experiments/bestbuyapi/app/scripts/services.js
@@ -9,8 +9,8 @@ angular.module('bestBuy.services', [])
     */
 
     bestBuyAPI.getCategories = function() {
-      return $http.get('http://www.bestbuy.ca/api/v2/json/category/Departments');
-    }
+      return $http.get('https://www.bestbuy.ca/api/v2/json/category/Departments');
+    };
 
     /*
     * @func getCategoryProducts
@@ -18,8 +18,8 @@ angular.module('bestBuy.services', [])
     * query all products in selected category
     */
     bestBuyAPI.getCategoryProducts = function(categoryId) {
-      return $http.get('http://www.bestbuy.ca/api/v2/json/search?categoryid='+categoryId);
-    }
+      return $http.get('https://www.bestbuy.ca/api/v2/json/search?categoryid='+categoryId);
+    };
 
     /*
     * @func getProduct
@@ -27,8 +27,8 @@ angular.module('bestBuy.services', [])
     * query all details for selected product
     */
     bestBuyAPI.getProduct = function(productId) {
-      return $http.get('http://www.bestbuy.ca/api/v2/json/product/'+productId);
-    }
+      return $http.get('https://www.bestbuy.ca/api/v2/json/product/'+productId);
+    };
 
     return bestBuyAPI;
   });
