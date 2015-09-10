0b1ada23d486f357207d388d175a837b0fd6ba36
Chris Bautista <christopher.x.bautista@hsbc.ca>
2015-09-10 10:48:46 -0700

    checkout 09092015

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ function InitService() {
 
             sortable.sort(function(a, b)
             {
-                return a[1]-b[1];
+                return b[1]-a[1];
             });
 
             return sortable; 
