cb4d394e16af419e475955bfb089134ce23dfbc6
Chris Bautista <christopher.bautista@gmail.com>
2017-01-26 05:00:36 -0800

    select filter

--- projects/filterjs/src/app.js
+++ projects/filterjs/src/app.js
@@ -1,11 +1,19 @@
+/**
+ * Filter Class
+ * requires lodash
+ */
 var FilterClass = (function () {
-    function FilterClass(parent, filters) {
+    function FilterClass(parent, filters, filterTypes) {
         if (filters === void 0) { filters = null; }
+        if (filterTypes === void 0) { filterTypes = ['select', 'text']; }
         this.config = {};
-        this.parentSelector = parent || '.list';
         this.itemSelector = '.list-items';
         this.filterSelector = '#filter';
         this.filterId = 'filter';
+        this.searchId = 'keyword-filter';
+        this.filterTypes = filterTypes;
+        this.parentSelector = parent || '.list';
+        this.minimumCharacters = 2;
         blocks = Array.from(document.querySelectorAll(this.parentSelector + " " + this.itemSelector));
         this.blocks = blocks;
         if (blocks.length > 0) {
@@ -29,31 +37,57 @@ var FilterClass = (function () {
         var options = _.reduce(this.config.filters, function (options, item, key) {
             return options + "<option value=\"" + item + "\"> " + item + " </option>";
         }, '');
-        var template = "\n            <div class=\"container\">\n                <div class=\"row\">\n                    <div class=\"col-md-5\">\n                        <select name=\"" + this.filterId + "\" id=\"" + this.filterId + "\" class=\"form-control\">\n                        " + options + "\n                        </select>\n                    </div>\n                </div>\n            </div>\n        ";
+        var template = "";
+        if (_.indexOf(this.filterTypes, 'text') !== -1) {
+            template += "\n            <div class=\"container\">\n                <div class=\"row\">\n                    <div class=\"col-md-5\">\n                    <input type=\"search\" value=\"\" name=\"keyword-filter\" id=\"keyword-filter\" class=\"form-control\" placeholder=\"enter keyword\">            \n                    </div>\n                </div>\n            </div>\n            ";
+        }
+        if (_.indexOf(this.filterTypes, 'select') !== -1) {
+            template += "\n            <div class=\"container\">\n                <div class=\"row\">\n                    <div class=\"col-md-5\">\n                        <select name=\"" + this.filterId + "\" id=\"" + this.filterId + "\" class=\"form-control\">\n                        " + options + "\n                        </select>\n                    </div>\n                </div>\n            </div>\n        ";
+        }
         insertBefore(template, parent.parentNode);
     };
     FilterClass.prototype.handlers = function () {
         var _this = this;
         var filterSelect = document.getElementById(this.filterId);
+        var filterKeyword = document.getElementById(this.searchId);
         filterSelect.addEventListener('change', function (e) {
-            _this.filterCallback(e);
+            _this.filterSelectCallback(e);
+        });
+        filterKeyword.addEventListener('keyup', function (e) {
+            _this.filterTextCallback(e);
         });
     };
     // callbacks
-    FilterClass.prototype.filterCallback = function (e) {
+    FilterClass.prototype.filterSelectCallback = function (e) {
         var filterString = e.target.options[e.target.selectedIndex].value;
+        this.filterNodes(filterString);
+    };
+    FilterClass.prototype.filterTextCallback = function (e) {
+        var filterString = e.target.value;
+        console.log('keyword', filterString.length, this.minimumCharacters, filterString.length > this.minimumCharacters);
+        if ((filterString.length > this.minimumCharacters) || (filterString.length === 0)) {
+            this.filterNodes(filterString);
+        }
+    };
+    FilterClass.prototype.filterNodes = function (filterString) {
         var element = this.blocks;
         console.log('filterString', filterString);
-        _.map(this.blocks, function (item) {
-            item.style.display = 'hide';
-        });
-        var divs = element.filter(function (item) {
-            if (item.dataset.filter.indexOf(filterString) !== -1) {
-                return true;
-            }
-        });
-        _.map(divs, function (item) {
-            item.style.display = 'block';
+        if (filterString.length > 0) {
+            this.utilUpdateNodesDisplay(this.blocks, 'none');
+            var divs = element.filter(function (item) {
+                if (item.dataset.filter.indexOf(filterString) !== -1) {
+                    return true;
+                }
+            });
+            this.utilUpdateNodesDisplay(divs, 'block');
+        }
+        else {
+            this.utilUpdateNodesDisplay(this.blocks, 'block');
+        }
+    };
+    FilterClass.prototype.utilUpdateNodesDisplay = function (nodes, style) {
+        _.map(nodes, function (item) {
+            item.style.display = style;
         });
     };
     return FilterClass;
--- projects/filterjs/src/app.ts
+++ projects/filterjs/src/app.ts
@@ -3,26 +3,38 @@ declare var document: any;
 declare var window: any;
 declare var domIsReady: any;
 
-
+/**
+ * Filter Class
+ * requires lodash
+ */
 class FilterClass {
     private blocks: any; // nodelist
     private filters: any;
     private config: any[];
+
 
     private parentSelector: string;
     private itemSelector: string;
 
     private filterSelector: string;
     private filterId: string;
+    private searchId: string;
+    private filterTypes: string[];
+    private minimumCharacters: number;
 
-    constructor(parent: any, filters: any[] = null) {
+
+    constructor(parent: any, filters: any[] = null, filterTypes: string[] = ['select', 'text']) {
 
         this.config = {};
 
-        this.parentSelector = parent || '.list';
         this.itemSelector = '.list-items';
         this.filterSelector = '#filter';
         this.filterId = 'filter';
+        this.searchId = 'keyword-filter';
+
+        this.filterTypes = filterTypes;
+        this.parentSelector = parent || '.list';
+        this.minimumCharacters = 2;
 
         blocks = Array.from(document.querySelectorAll(`${this.parentSelector} ${this.itemSelector}`));
 
@@ -60,7 +72,21 @@ class FilterClass {
             return `${options}<option value="${item}"> ${item} </option>`;
         }, '');
 
-        let template = `
+        let template = "";
+
+        if (_.indexOf(this.filterTypes, 'text') !== -1) {
+            template += `
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5">
+                    <input type="search" value="" name="keyword-filter" id="keyword-filter" class="form-control" placeholder="enter keyword">            
+                    </div>
+                </div>
+            </div>
+            `
+        }
+        if (_.indexOf(this.filterTypes, 'select') !== -1) {
+            template += `
             <div class="container">
                 <div class="row">
                     <div class="col-md-5">
@@ -71,6 +97,7 @@ class FilterClass {
                 </div>
             </div>
         `;
+        }
 
         insertBefore(template, parent.parentNode);
 
@@ -79,36 +106,59 @@ class FilterClass {
     handlers() {
 
         let filterSelect: any = document.getElementById(this.filterId);
+        let filterKeyword: any = document.getElementById(this.searchId);
         filterSelect.addEventListener('change', (e) => {
-            this.filterCallback(e)
+            this.filterSelectCallback(e)
+        })
+
+        filterKeyword.addEventListener('keyup', (e) => {
+            this.filterTextCallback(e)
         })
 
     }
 
     // callbacks
-    filterCallback(e) {
+    filterSelectCallback(e) {
         let filterString = e.target.options[e.target.selectedIndex].value;
+        this.filterNodes(filterString);
+    }
+
+    filterTextCallback(e) {
+        let filterString = e.target.value;
+        console.log('keyword', filterString.length, this.minimumCharacters, filterString.length > this.minimumCharacters)
+        if ((filterString.length > this.minimumCharacters) || (filterString.length === 0)) {
+            this.filterNodes(filterString);
+        }
+    }
+
+    filterNodes(filterString) {
         let element = this.blocks;
         console.log('filterString', filterString);
 
-        _.map(this.blocks, function (item) {
+        if (filterString.length > 0) {
+            this.utilUpdateNodesDisplay(this.blocks, 'none');
 
-            item.style.display = 'hide';
+            let divs = element.filter(function (item) {
+                if (item.dataset.filter.indexOf(filterString) !== -1) {
+                    return true;
+                }
+            });
 
-        });
-
-        let divs = element.filter(function (item) {
-            if (item.dataset.filter.indexOf(filterString) !== -1) {
-                return true;
-            }
-        });
-
-        _.map(divs, function (item) {
-            item.style.display = 'block';
-        });
+            this.utilUpdateNodesDisplay(divs, 'block');
+        } else {
+            this.utilUpdateNodesDisplay(this.blocks, 'block');
+        }
 
     }
 
+    utilUpdateNodesDisplay(nodes, style) {
+        _.map(nodes, function (item) {
+
+            item.style.display = style;
+
+        });
+
+    }
 
 }
 
